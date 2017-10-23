c4316c25e5e708f93597c60fec8f2be815d0bb86
Ben Shi <ben.shi@spreadtrum.com>
2017-10-23 15:28:03 +0800

    Modify test case according to changes in CL 71992.

--- typepromotion_test.go
+++ typepromotion_test.go
@@ -12,17 +12,17 @@ import (
 )
 
 //go:noinline
-func typeProAdd(a, b []int8, c []int32) {
-	c[0] = int32(a[0]+b[0]) + c[0]
-	c[1] = int32(a[1]+b[1]) + c[1]
-	c[2] = int32(a[2]+b[2]) + c[2]
-	c[3] = int32(a[3]+b[3]) + c[3]
-	c[4] = int32(a[4]+b[4]) + c[4]
-	c[5] = int32(a[5]+b[5]) + c[5]
-	c[6] = int32(a[6]+b[6]) + c[6]
-	c[7] = int32(a[7]+b[7]) + c[7]
-	c[8] = int32(a[8]+b[8]) + c[8]
-	c[9] = int32(a[9]+b[9]) + c[9]
+func typeProAdd(a, b []int8) {
+	c[0] = int32(a[0] + b[0])
+	c[1] = int32(a[1] + b[1])
+	c[2] = int32(a[2] + b[2])
+	c[3] = int32(a[3] + b[3])
+	c[4] = int32(a[4] + b[4])
+	c[5] = int32(a[5] + b[5])
+	c[6] = int32(a[6] + b[6])
+	c[7] = int32(a[7] + b[7])
+	c[8] = int32(a[8] + b[8])
+	c[9] = int32(a[9] + b[9])
 }
 
 var a0, b0 []int8
