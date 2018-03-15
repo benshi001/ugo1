f8b1667d453beb0af502c31063af9959ae1c3dec
Ben Shi <powerman1st@163.com>
2018-03-15 06:32:53 +0000

    Add a new test case.

---
+++ fmlamls_test.go
@@ -0,0 +1,23 @@
+// Copyright 2017 Ben Shi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This benchmark tests the performance of frequent -(a*b) calculations.
+// It shows the improvement in https://go-review.googlesource.com/c/go/+/61150
+
+package go1
+
+import "testing"
+
+//go:noinline
+func fmlamls(a, b, c, d float64) (float64, float64, float64, float64, float64, float64, float64, float64) {
+	return c - a*b, c + a*b, b - a*d, b + a*d, c - b*d, c + b*d, a + c*d, a - c*d
+}
+
+func BenchmarkMLAMLS(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 4096; j++ {
+			_, _, _, _, _, _, _, _ = fmlamls(12, 45, -23, -234)
+		}
+	}
+}
