14030a8ec03a71d64455f29d9173ce2b6c433f87
Ben Shi <powerman1st@163.com>
2018-07-10 10:14:24 +0800

    add a new test case

---
+++ tstteq2_test.go
@@ -0,0 +1,131 @@
+// Copyright 2018 Ben Shi. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package go1
+
+// This benchmark tests the performance of the comparason of x&y to 0 and x^y to 0.
+// It shows improvement in https://go-review.googlesource.com/c/go/+/122855
+
+import (
+	"testing"
+)
+
+var a0 int32
+
+func init() {
+	a0 = 0
+}
+
+//go:noinline
+func inc() {
+	a0++
+}
+
+//go:noinline
+func tstteq(x int32) {
+	if x&0x000000ff >= 0 {
+		inc()
+	}
+	if x&0x0000ff00 >= 0 {
+		inc()
+	}
+	if x&0x00ff0000 >= 0 {
+		inc()
+	}
+	if x&0x7f000000 >= 0 {
+		inc()
+	}
+	if x^0x000000ff >= 0 {
+		inc()
+	}
+	if x^0x0000ff00 >= 0 {
+		inc()
+	}
+	if x^0x00ff0000 >= 0 {
+		inc()
+	}
+	if x^0x7f000000 >= 0 {
+		inc()
+	}
+	if x&0x000000ff <= 0 {
+		inc()
+	}
+	if x&0x0000ff00 <= 0 {
+		inc()
+	}
+	if x&0x00ff0000 <= 0 {
+		inc()
+	}
+	if x&0x7f000000 <= 0 {
+		inc()
+	}
+	if x^0x000000ff <= 0 {
+		inc()
+	}
+	if x^0x0000ff00 <= 0 {
+		inc()
+	}
+	if x^0x00ff0000 <= 0 {
+		inc()
+	}
+	if x^0x7f000000 <= 0 {
+		inc()
+	}
+	if x&0x000000ff > 0 {
+		inc()
+	}
+	if x&0x0000ff00 > 0 {
+		inc()
+	}
+	if x&0x00ff0000 > 0 {
+		inc()
+	}
+	if x&0x7f000000 > 0 {
+		inc()
+	}
+	if x^0x000000ff > 0 {
+		inc()
+	}
+	if x^0x0000ff00 > 0 {
+		inc()
+	}
+	if x^0x00ff0000 > 0 {
+		inc()
+	}
+	if x^0x7f000000 > 0 {
+		inc()
+	}
+	if x&0x000000ff < 0 {
+		inc()
+	}
+	if x&0x0000ff00 < 0 {
+		inc()
+	}
+	if x&0x00ff0000 < 0 {
+		inc()
+	}
+	if x&0x7f000000 < 0 {
+		inc()
+	}
+	if x^0x000000ff < 0 {
+		inc()
+	}
+	if x^0x0000ff00 < 0 {
+		inc()
+	}
+	if x^0x00ff0000 < 0 {
+		inc()
+	}
+	if x^0x7f000000 < 0 {
+		inc()
+	}
+}
+
+func BenchmarkTSTTEQ(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for j := 0; j < 16384; j++ {
+			tstteq(0)
+		}
+	}
+}
